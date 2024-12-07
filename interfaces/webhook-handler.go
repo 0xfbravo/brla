832491f1f3294d66add47ca2b3ee417f632d5f8a
Fellipe Bravo <fellipe.bravo@mobiup.com.br>
2024-12-07 09:20:26 -0300

    Adicionado `context` ao webhook handler

--- interfaces/webhook-handler.go
+++ interfaces/webhook-handler.go
@@ -1,19 +1,20 @@
 package interfaces
 
 import (
+	"context"
 	"github.com/0xfbravo/brla/model"
 )
 
 type WebhookHandler interface {
 	// HandleQueued handles the `queued` webhook response.
-	HandleQueued(req *model.WebhookResponse) error
+	HandleQueued(c context.Context, req *model.WebhookResponse) error
 
 	// HandlePosted handles the posted webhook response
-	HandlePosted(req *model.WebhookResponse) error
+	HandlePosted(c context.Context, req *model.WebhookResponse) error
 
 	// HandleSuccess handles the success webhook response
-	HandleSuccess(req *model.WebhookResponse) error
+	HandleSuccess(c context.Context, req *model.WebhookResponse) error
 
 	// HandleFailed handles the failed webhook response
-	HandleFailed(req *model.WebhookResponse) error
+	HandleFailed(c context.Context, req *model.WebhookResponse) error
 }
