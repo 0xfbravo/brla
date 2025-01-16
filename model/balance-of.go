8314bc8007c188c8001e48b0b5a83226aac189a3
Fellipe Bravo <fellipe.bravo@gmail.com>
2025-01-15 23:14:03 -0300

    Correção na visibilidade de atributos de balance

--- model/balance-of.go
+++ model/balance-of.go
@@ -1,6 +1,6 @@
 package model
 
 type BalanceOf struct {
-	Balance    int64  `json:"balance,omitempty" validate:"required" example:"100000000"`
-	BalanceWei string `json:"balanceWei,omitempty" validate:"required" example:"1000000000000000000"`
+	Balance    int64  `json:"balance" validate:"required" example:"100000000"`
+	BalanceWei string `json:"balanceWei" validate:"required" example:"1000000000000000000"`
 }
